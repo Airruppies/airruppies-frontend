2be98f00c48adb89856e08c4c2663cc85ac24a92
Timothyanekwu <timo2020thy@gmail.com>
2024-06-29 16:41:16 +0100

    app.js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import RegistrationPage from './components/RegistrationPage';
-import VerifyEmailPage from './components/VerifyEmailPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import RegistrationPage from "./components/RegistrationPage";
+import VerifyEmailPage from "./components/VerifyEmailPage";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/verify-email" element={<VerifyEmailPage />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
