016ed552e61bf3fefe9d8cb209a7609055b7db78
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2024-06-30 05:50:10 +0100

    Code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import RegistrationPage from "./components/RegistrationPage";
-import VerifyEmailPage from "./components/VerifyEmailPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import RegistrationPage from './RegistrationPage';
+import VerifyEmailPage from './VerifyEmailPage';
+import NotFoundPage from './NotFoundPage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/verify-email" element={<VerifyEmailPage />} />
-      </Routes>
-      {/*  */}
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/verify-email" element={<VerifyEmailPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   define: {
     // eslint-disable-next-line no-undef
     'process.env': process.env
+
   }
-})
-
-// vite.config.js
-
-
-
+});
